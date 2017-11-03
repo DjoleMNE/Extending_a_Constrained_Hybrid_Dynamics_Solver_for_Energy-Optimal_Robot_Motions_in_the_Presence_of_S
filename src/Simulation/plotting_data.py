8cb9365ecf1ce3b1ffde4615080e9afa6b56969f
Djordje Vukcevic <djordje.vukcevic@smail.inf.h-brs.de>
2017-11-03 17:04:54 +0100

    Python code for plotting changed
    Trisurf function from matplotlib used

--- src/Simulation/plotting_data.py
+++ src/Simulation/plotting_data.py
@@ -4,54 +4,44 @@ from sympy import *
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-from matplotlib.ticker import LinearLocator
-import matplotlib.patches as mpatches
-from matplotlib.collections import PatchCollection
-import matplotlib.path as mpath
+import pandas as pd
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
 
 input_data = np.loadtxt("plot_data.txt", dtype='double', delimiter=' ')
-# print(input_data.shape[0])
 rows = input_data.shape[0]
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
 
 # Make data.
 X = input_data[:-1,0]
 xlen = len(X)
 Y = input_data[:-1,1]
 ylen = len(Y)
-X, Y = np.meshgrid(X, Y)
 Z = input_data[:-1,2]
 
+df = pd.DataFrame({'x': X, 'y': Y, 'z': Z})
 
-# Create an empty array of strings with the same shape as the meshgrid, and
-# populate it with two colors in a checkerboard pattern.
-# colortuple = ('r', 'b')
-# colors = np.empty(X.shape, dtype=str)
-#
-# for y in range(ylen):
-#     for x in range(xlen):
-#         colors[x, y] = colortuple[(x + y) % len(colortuple)]
+fig = plt.figure()
+ax = Axes3D(fig)
 
-# Plot the surface with face colors taken from the array we made.
-surf = ax.plot_surface(X, Y, Z, linewidth=0)
-# surf = ax.plot_surface(X, Y, Z, facecolors=colors, linewidth=0)
+surf = ax.plot_trisurf(df.x, df.y, df.z, cmap = cm.jet, linewidth = 0.1)
+fig.colorbar(surf, shrink=0.5, aspect=5)
 
-# Customize axes.
-ax.set_zlim(0, input_data[-1,2]+100)
-ax.set_xlim(-45, 45)
-ax.set_ylim(-25, 25)
 ax.set_xlabel('Tau_0')
 ax.set_ylabel('Tau_1')
 ax.set_zlabel('Acceleration energy')
 
-ax.w_zaxis.set_major_locator(LinearLocator(6))
+class Arrow3D(FancyArrowPatch):
 
-# Add a color bar which maps values to colors.
-# fig.colorbar(surf, shrink=0.5, aspect=5)
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
 
-# circle1 = plt.Circle((input_data[-1,0], input_data[-1,1]), 2, color='r')
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        FancyArrowPatch.draw(self, renderer)
 
-# ax.add_artist(circle1)
+a = Arrow3D([input_data[-1,0], input_data[-1,0]], [input_data[-1,1], input_data[-1,1]], [input_data[-1,2], input_data[-1,2]+input_data[-1,2]/10], mutation_scale=20,  lw=1, arrowstyle="-|>", color="k")
+ax.add_artist(a)
 plt.show()
